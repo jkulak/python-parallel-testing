0bb698cc89c779118a5a001ef7fde101d1f86457
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-15 18:28:02 +0100

    Commented out API call and added return statement to query_api function

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
-import time
-
-import requests
-
-API_URL = "https://jsonplaceholder.typicode.com/"
+# API_URL = "https://jsonplaceholder.typicode.com/"
 
 
 def query_api(user_input):
-    response = requests.get(f"{API_URL}?query={user_input}", timeout=5)
-
-    # sleep for 2 seconds
-    time.sleep(2)
-
-    return "just a test"
+    # response = requests.get(f"{API_URL}?query={user_input}", timeout=5)
+    return user_input
 
 
 print(query_api("test"))
