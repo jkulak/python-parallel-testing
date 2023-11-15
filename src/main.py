0137dd445a683f6fe5ea920832568df184caf269
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-15 17:52:07 +0100

    Add timeout and sleep to query_api function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+import time
+
 import requests
 
 API_URL = "https://jsonplaceholder.typicode.com/"
 
+
 def query_api(user_input):
-    response = requests.get(f"{API_URL}?query={user_input}")
-    return response.json()
+    response = requests.get(f"{API_URL}?query={user_input}", timeout=5)
+
+    # sleep for 2 seconds
+    time.sleep(2)
+
+    return "just a test"
+
+
+print(query_api("test"))
