2b5d755e98819dbaa84ccd8181f5902297f19fb0
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-15 18:07:31 +0100

    aider: Refactored test_main.py to run test_query_api method separately in parallel using ThreadPoolExecutor.
    # Aider chat conversation:
    
    USER: tests should use an array of input and expected export. test, for each text data row should be run separately in parallel
    
    ASSISTANT: To run tests in parallel, we can use the `concurrent.futures` module in Python. This module provides a high-level interface for asynchronously executing callables. We can use the `ThreadPoolExecutor` class to create a pool of worker threads that can execute the tests in parallel.
    
    However, to implement this, we need to modify the `src/test_main.py` file. Here are the changes:
    
    <source>python
    src/test_main.py
    <<<<<<< SEARCH
    import unittest
    import requests_mock
    import main
    
    class TestMain(unittest.TestCase):
        def test_query_api(self):
            with requests_mock.Mocker() as m:
                m.get("https://jsonplaceholder.typicode.com/?query=test", text='api response')
                self.assertEqual(main.query_api("test"), 'api response')
    
    if __name__ == '__main__':
        unittest.main()
    =======
    import unittest
    import requests_mock
    import main
    from concurrent.futures import ThreadPoolExecutor
    
    class TestMain(unittest.TestCase):
        def test_query_api(self, test_input, expected_output):
            with requests_mock.Mocker() as m:
                m.get(f"https://jsonplaceholder.typicode.com/?query={test_input}", text=expected_output)
                self.assertEqual(main.query_api(test_input), expected_output)
    
    if __name__ == '__main__':
        test_data = [
            ("test1", "api response1"),
            ("test2", "api response2"),
            # add more test data as needed
        ]
    
        with ThreadPoolExecutor() as executor:
            for test_input, expected_output in test_data:
                executor.submit(unittest.main().test_query_api(test_input, expected_output))
    >>>>>>> REPLACE
    </source>
    
    Please note that the `unittest` module is not designed to support parallel execution of tests out of the box. The above code may not work as expected. For a more robust solution, you might want to consider using a testing framework that supports parallel execution, such as `pytest` with the `pytest-xdist` plugin.

--- src/test_main.py
+++ src/test_main.py
@@ -1,12 +1,21 @@
 import unittest
 import requests_mock
 import main
+from concurrent.futures import ThreadPoolExecutor
 
 class TestMain(unittest.TestCase):
-    def test_query_api(self):
+    def test_query_api(self, test_input, expected_output):
         with requests_mock.Mocker() as m:
-            m.get("https://jsonplaceholder.typicode.com/?query=test", text='api response')
-            self.assertEqual(main.query_api("test"), 'api response')
+            m.get(f"https://jsonplaceholder.typicode.com/?query={test_input}", text=expected_output)
+            self.assertEqual(main.query_api(test_input), expected_output)
 
 if __name__ == '__main__':
-    unittest.main()
+    test_data = [
+        ("test1", "api response1"),
+        ("test2", "api response2"),
+        # add more test data as needed
+    ]
+
+    with ThreadPoolExecutor() as executor:
+        for test_input, expected_output in test_data:
+            executor.submit(unittest.main().test_query_api(test_input, expected_output))
