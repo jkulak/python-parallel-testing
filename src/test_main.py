2aed811e6ebf24f6a8f83c2f5512604c91bb24e9
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-15 17:57:57 +0100

    aider: Refactored test_main.py to make the test discoverable by adding a conditional statement at the end of the file to call the unittest's main function.
    # Aider chat conversation:
    
    USER: python-parallel-testing (main) ✗ docker run -it --rm -v $(pwd)/src:/app --name my-running-app my-python-app python -m unittest ./test_main.py
    just a test
    
    ----------------------------------------------------------------------
    Ran 0 tests in 0.000s
    
    ASSISTANT: It seems like the test is not being discovered by the unittest module. This could be due to the file structure or the way the test is written.
    
    Let's modify the `src/test_main.py` file to ensure that the test is discoverable. We will add a conditional statement at the end of the file to call the unittest's main function. This will allow the tests to be run when the script is run directly.
    
    Here is the *SEARCH/REPLACE block* for the `src/test_main.py` file:
    
    <source>python
    src/test_main.py
    <<<<<<< SEARCH
    import requests_mock
    import main
    
    def test_query_api():
        with requests_mock.Mocker() as m:
            m.get("https://jsonplaceholder.typicode.com/?query=test", text='api response')
            assert main.query_api("test") == 'api response'
    =======
    import unittest
    import requests_mock
    import main
    
    class TestMain(unittest.TestCase):
        def test_query_api(self):
            with requests_mock.Mocker() as m:
                m.get("https://jsonplaceholder.typicode.com/?query=test", text='api response')
                self.assertEqual(main.query_api("test"), 'api response')
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> REPLACE
    </source>
    
    This change wraps the test function inside a class that inherits from `unittest.TestCase` and adds a conditional statement at the end of the file to call the unittest's main function. This will allow the tests to be run when the script is run directly.

--- src/test_main.py
+++ src/test_main.py
@@ -1,7 +1,12 @@
+import unittest
 import requests_mock
 import main
 
-def test_query_api():
-    with requests_mock.Mocker() as m:
-        m.get("https://jsonplaceholder.typicode.com/?query=test", text='api response')
-        assert main.query_api("test") == 'api response'
+class TestMain(unittest.TestCase):
+    def test_query_api(self):
+        with requests_mock.Mocker() as m:
+            m.get("https://jsonplaceholder.typicode.com/?query=test", text='api response')
+            self.assertEqual(main.query_api("test"), 'api response')
+
+if __name__ == '__main__':
+    unittest.main()
