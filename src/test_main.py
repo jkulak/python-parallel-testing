f7b886178059d79bfc047018d676d3491661a9f7
Jakub Kułak <jakub.kulak@gmail.com>
2023-11-15 18:10:33 +0100

    aider: Added 20 diverse test cases to the `test_data` list in `src/test_main.py` using meat and food related words.
    # Aider chat conversation:
    
    USER: in test_data ad 20 more examples, make them diverse, use meat and food related words
    
    ASSISTANT: To add more test data to the `src/test_main.py` file, we will need to modify the `test_data` list. Here is how we can do it:
    
    <source>python
    src/test_main.py
    <<<<<<< SEARCH
    test_data = [
        ("test1", "api response1"),
        ("test2", "api response2"),
        # add more test data as needed
    ]
    =======
    test_data = [
        ("test1", "api response1"),
        ("test2", "api response2"),
        ("beef", "api response for beef"),
        ("chicken", "api response for chicken"),
        ("pork", "api response for pork"),
        ("fish", "api response for fish"),
        ("lamb", "api response for lamb"),
        ("turkey", "api response for turkey"),
        ("duck", "api response for duck"),
        ("veal", "api response for veal"),
        ("salmon", "api response for salmon"),
        ("tuna", "api response for tuna"),
        ("shrimp", "api response for shrimp"),
        ("lobster", "api response for lobster"),
        ("crab", "api response for crab"),
        ("bacon", "api response for bacon"),
        ("ham", "api response for ham"),
        ("sausage", "api response for sausage"),
        ("steak", "api response for steak"),
        ("ribs", "api response for ribs"),
        ("burger", "api response for burger"),
        ("pizza", "api response for pizza"),
        ("pasta", "api response for pasta"),
        ("rice", "api response for rice"),
        ("bread", "api response for bread"),
    ]
    >>>>>>> REPLACE
    </source>
    
    This will add 20 more diverse test cases to the `test_data` list, using meat and food related words. The expected output for each test case is set to "api response for [food item]", but you may need to adjust these to match the actual expected responses from the API.

--- src/test_main.py
+++ src/test_main.py
@@ -5,7 +5,29 @@ import main
 test_data = [
     ("test1", "api response1"),
     ("test2", "api response2"),
-    # add more test data as needed
+    ("beef", "api response for beef"),
+    ("chicken", "api response for chicken"),
+    ("pork", "api response for pork"),
+    ("fish", "api response for fish"),
+    ("lamb", "api response for lamb"),
+    ("turkey", "api response for turkey"),
+    ("duck", "api response for duck"),
+    ("veal", "api response for veal"),
+    ("salmon", "api response for salmon"),
+    ("tuna", "api response for tuna"),
+    ("shrimp", "api response for shrimp"),
+    ("lobster", "api response for lobster"),
+    ("crab", "api response for crab"),
+    ("bacon", "api response for bacon"),
+    ("ham", "api response for ham"),
+    ("sausage", "api response for sausage"),
+    ("steak", "api response for steak"),
+    ("ribs", "api response for ribs"),
+    ("burger", "api response for burger"),
+    ("pizza", "api response for pizza"),
+    ("pasta", "api response for pasta"),
+    ("rice", "api response for rice"),
+    ("bread", "api response for bread"),
 ]
 
 @pytest.mark.parametrize("test_input,expected_output", test_data)
